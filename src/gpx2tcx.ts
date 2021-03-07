9b8a2b380e81f8634f8d45d87c454c21c1d6679c
Pat Lehay <pat@patleahy.com>
2021-03-07 15:52:46 -0800

    custom function to calculate DistanceMeters

--- src/gpx2tcx.ts
+++ src/gpx2tcx.ts
@@ -1,5 +1,8 @@
 import { from } from './lir';
 
+
+
+
 export const rules = from('gpx.metadata.name').to('TrainingCenterDatabase.Courses.Course.Name')
     .from('gpx.trk.trkseg.trkpt').each()
     .to('TrainingCenterDatabase.Courses.Course.Track.Trackpoint')
@@ -8,3 +11,23 @@ export const rules = from('gpx.metadata.name').to('TrainingCenterDatabase.Course
             .from('@_lon').to('Position.LongitudeDegrees')
             .from('time').to('Time')
             .from('ele').to('AltitudeMeters'));
+
+
+function haversine(lat1, lon1, lat2, lon2) {
+    return 42.0;
+}
+
+var distance = 0.0;
+var prevPoint = undefined;
+function calcDistance(point: any): any {
+    if (prevPoint) {
+        distance += haversine(prevPoint['@_lat'], prevPoint['@_lon'], point['@_lat'], point['@_lon']);
+    }
+    prevPoint = point;
+    return { 'DistanceMeters' : distance };
+}
+
+rules.from('gpx.trk.trkseg.trkpt')
+    .each()
+    .using(calcDistance)
+    .to('TrainingCenterDatabase.Courses.Course.Track.Trackpoint');
