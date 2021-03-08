bb7aad90f1d454e197918e40ebecf74cd001345c
Pat Lehay <pat@patleahy.com>
2021-03-08 12:26:16 -0800

    Add comment

--- src/lir-cli.ts
+++ src/lir-cli.ts
@@ -1,3 +1,14 @@
+/**
+ * A command line script for using gpx2tcx and rss2atom to convert XML files.
+ * 
+ * To convert a GPX (GPS Exchange Format) to [TCX (Training Center XML) run:
+ * 
+ *      npm run cli gpx2tcs <input.gpx> <output.tcx>
+ *
+ * To convert an RSS file to Atom:
+ * 
+ *      npm run cli rss2atom <input.rss> <output.atom>
+ */
 import parser = require('fast-xml-parser');
 import fs = require('fs');
 import rss2atom = require('./rss2atom');
@@ -20,9 +31,6 @@ function main(argv: string[]) {
     const objOut = rules.map(objIn);
     const xmlOut = (new parser.j2xParser(parserOptions)).parse(objOut);
     fs.writeFileSync(outputPath, xmlOut, 'utf8');
-
-    console.log(objOut);
-    console.log(xmlOut);
 
     console.log(`${fileType}: ${inputPath} -> ${outputPath}`);
 }
